27dddfb5614ee1a6942d1be4b888203eef504fc3
Cardoso Matine <cardoso.matine27@gmail.com>
2025-07-27 00:32:36 +0200

    Add basic frontend for contact app with list and create views

--- contacts/models.py
+++ contacts/models.py
@@ -1,14 +1,10 @@
 from django.db import models
 
-# Create your models here.
-
-from django.db import models
-
-# Modelo simples de contato
 class Contact(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField()
     phone = models.CharField(max_length=20)
+    email = models.EmailField()
 
     def __str__(self):
         return self.name
+
--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Contact
 
-# Create your views here.
+def contact_list(request):
+    contacts = Contact.objects.all()
+    return render(request, 'contacts/contact_list.html', {'contacts': contacts})
+
+def contact_create(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        phone = request.POST['phone']
+        email = request.POST['email']
+        Contact.objects.create(name=name, phone=phone, email=email)
+        return redirect('contact_list')
+    return render(request, 'contacts/contact_form.html')
